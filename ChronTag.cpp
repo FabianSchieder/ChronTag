6d50b9e89b65cd5be0c873886e7fd42054979102
vujh <officialvujh@gmail.com>
2024-09-30 21:56:42 +0200

    first commit

---
+++ ChronTag.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 30.09.2024.
+//
+
+#include "ChronTag.h"
---
+++ ChronTag.h
@@ -0,0 +1,17 @@
+//
+// Created by admin on 30.09.2024.
+//
+
+#ifndef CHRONTAG_H
+#define CHRONTAG_H
+
+
+
+class ChronTag
+{
+
+};
+
+
+
+#endif //CHRONTAG_H
